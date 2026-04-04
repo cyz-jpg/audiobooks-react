8752515234ef65cc9101db06e5a4f0711c1b2663
chao <chao.zhao@ugent.be>
2026-04-04 11:39:51 +0200

    all routes are done

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,16 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import './index.css'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
-
-const apiUrl = import.meta.env.VITE_API_URL
+import Audiobooks from './pages/model/Audiobooks'
+import Genres from './pages/model/Genres'
+import Playbackpositions from './pages/model/Playbackpositions'
+import Ratings from './pages/model/Ratings'
+import Users from './pages/model/Users'
+import Audiobook from './pages/object/Audiobook'
+import Genre from './pages/object/Genre'
+import Playbackposition from './pages/object/Playbackposition'
+import Rating from './pages/object/Rating'
+import User from './pages/object/User'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,11 +21,21 @@ createRoot(document.getElementById('root')).render(
       <nav>
         <Link to="/">Home</Link>
       </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/audiobooks" element={<Audiobooks />} />
+        <Route path="/genres" element={<Genres />} />
+        <Route path="/playbackpositions" element={<Playbackpositions />} />
+        <Route path="/ratings" element={<Ratings />} />
+        <Route path="/users/:encodedUrl" element={<User />} />
+        <Route path="/audiobooks/:encodedUrl" element={<Audiobook />} />
+        <Route path="/genres/:encodedUrl" element={<Genre />} />
+        <Route path="/playbackpositions/:encodedUrl" element={<Playbackposition />} />
+        <Route path="/ratings/:encodedUrl" element={<Rating />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-
     </BrowserRouter>
   </StrictMode>,
 )
