d99db338c4b06171edf8eab620a175a35841f36f
chao <chao.zhao@ugent.be>
2026-04-04 20:13:41 +0200

    react conventies + refactoring

--- src/pages/model/Reviews.jsx
+++ src/pages/model/Reviews.jsx
@@ -1,10 +1,10 @@
 import { useLocation } from 'react-router-dom'
 
-export default function Playbackpositions() {
+export default function Reviews() {
   const location = useLocation()
   const apiUrl = location.state?.apiUrl
 
   console.log(apiUrl)
 
-  return <h1>Playbackpositions</h1>
+  return <h1>Reviews</h1>
 }
--- src/pages/object/Playbackposition.jsx
+++
@@ -1,3 +0,0 @@
-export default function Playbackposition() {
-  return <h1>Playbackposition</h1>
-}
